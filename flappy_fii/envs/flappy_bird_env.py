8805ec5fcdaffa8f475a743222ec012696ff1628
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-12-21 18:43:47 +0200

    change display to 60 fps

--- flappy_fii/envs/flappy_bird_env.py
+++ flappy_fii/envs/flappy_bird_env.py
@@ -82,7 +82,7 @@ class FlappyBirdEnv(gymnasium.Env):
             be drawn.
     """
 
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 30}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 60}
 
     def __init__(
         self,
